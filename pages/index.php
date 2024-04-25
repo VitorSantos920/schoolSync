84539bbbbb260d453832f22c08e6862ecf96ea1a
Maria Natália <cardim.natalia@gmail.com>
2024-04-24 21:20:19 -0300

    Alteracao na estrutura do head

--- pages/index.php
+++ pages/index.php
@@ -72,9 +72,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../assets/css/bootstrap.min.css">
     <link rel="stylesheet" href="../assets/css/global.css">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login SchoolSync</title>
 </head>
 <body>
