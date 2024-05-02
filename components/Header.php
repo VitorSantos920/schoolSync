67aea5fa033fe4e22eb817749a9a4a37bd79dbd8
VitorMaxx <maximianoe989@gmail.com>
2024-05-01 22:49:27 -0300

    criei o header

---
+++ components/Header.php
@@ -0,0 +1,45 @@
+<head>
+<link rel="stylesheet" type="text/css" href="../assets/css/bootstrap.min.css"/>
+
+<link rel="stylesheet" type="text/css" href="../assets/css/header.css"/>
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/5.0.0-3/css/ionicons.min.css">
+
+</head>
+
+<aside id="tudo">
+    <div class="logo-container">
+        <a id="logo" href="/">Painel</a>
+    </div>
+
+    <ul class="headerList">
+
+        <li>
+            <a href="">
+                <!-- Ícone de uma barra de pesquisa da Ionicons -->
+                <i class="fas fa-search"></i>   
+            </a>
+        </li>
+        <li>
+            <a href="">
+                <!-- Ícone da lua do Font Awesome -->
+                <i class="fas fa-moon"></i>   
+            </a>
+        </li>
+        <li>
+            <a href="">
+                <!-- Ícone de seta para baixo do Font Awesome -->
+                <i class="fas fa-chevron-down"></i>   
+            </a>
+        </li>
+        
+        <li>
+            <a href="">
+                <i class="fas fa-bell"></i>   
+            </a>
+        </li>
+    </ul>
+</aside>
+
