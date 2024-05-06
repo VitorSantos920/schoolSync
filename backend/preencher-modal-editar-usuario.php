b6c03d5fda78e9fff0a74e91560d254c132b799e
Vitor Santos <pireshugo737@gmail.com>
2024-05-06 08:41:57 -0300

    Consertando erro de digitação no case responsável

--- backend/preencher-modal-editar-usuario.php
+++ backend/preencher-modal-editar-usuario.php
@@ -37,7 +37,7 @@ function retornaDadosCompletosPorCategoria($dadosUsuario)
 
       break;
 
-    case 'Responsável':
+    case 'Responsavel':
       $dadosResponsavel = DB::queryFirstRow("SELECT res.cpf, res.telefone, res.quantidade_filho FROM responsavel res WHERE res.usuario_id = %i", $idUsuario);
       return array_merge($dadosUsuario, $dadosResponsavel);
 
@@ -69,111 +69,111 @@ function montaEstruturaFormulario($dadosCompletos)
   switch ($dadosCompletos['categoria']) {
     case 'Aluno':
       $html .= "
-        <fieldset>
-          <label for='ipt-escola-aluno' class='form-label'>Escola do Aluno</label>
-          <input type='text' name='ipt-escola-aluno' id='ipt-escola-aluno' class='form-control' value='{$dadosCompletos['escola']}' required>
-        </fieldset>
-        <fieldset>
-          <label for='ipt-dataNascimento-aluno' class='form-label'>Data de Nascimento</label>
-          <input type='date' name='ipt-dataNascimento-aluno' id='ipt-dataNascimento-aluno' class='form-control' value='{$dadosCompletos['data_nascimento']}' required>
-        </fieldset>
-        <fieldset>
-          <label for='select-categoria' class='form-label'>Categoria do Usuário</label>
-          <select class='form-control form-select' name='select-categoria' id='select-categoria' required>
-            <option value='0' selected disabled>Categoria</option>
-            <option value='Aluno' selected>Aluno</option>
-            <option value='Responsavel'>Responsável</option>
-            <option value='Administrador'>Administrador</option>
-            <option value='Professor'>Professor</option>
-          </select>
-        </fieldset>
-        <fieldset>
-          <label for='select-genero-aluno' class='form-label'>Gênero do Aluno</label>
-          <select class='form-control form-select' name='select-genero-aluno' id='select-genero-aluno' required>
-            <option value='0' selected disabled>Gênero</option>
-            <option value='Masculino' selected>Masculino</option>
-            <option value='Feminino'>Feminino</option>
-          </select>
-        </fieldset>
-        <fieldset>
-          <label for='select-escolaridade' class='form-label'>Escolaridade do Aluno</label>
-          <select class='form-control form-select' name='select-escolaridade' id='select-escolaridade' required> 
-            <option value='-1' selected disabled>Escolaridade</option>
-            <option value='1'>1</option>
-            <option value='2'>2</option>
-            <option value='3'>3</option>
-            <option value='4'>4</option>
-            <option value='5'>5</option>
-          </select>
-        </fieldset>
-      ";
+      <fieldset>
+        <label for='ipt-escola-aluno' class='form-label'>Escola do Aluno</label>
+        <input type='text' name='ipt-escola-aluno' id='ipt-escola-aluno' class='form-control' value='{$dadosCompletos['escola']}' required>
+      </fieldset>
+      <fieldset>
+        <label for='ipt-dataNascimento-aluno' class='form-label'>Data de Nascimento</label>
+        <input type='date' name='ipt-dataNascimento-aluno' id='ipt-dataNascimento-aluno' class='form-control' value='{$dadosCompletos['data_nascimento']}' required>
+      </fieldset>
+      <fieldset>
+        <label for='select-categoria' class='form-label'>Categoria do Usuário</label>
+        <select class='form-control form-select' name='select-categoria' id='select-categoria' required>
+          <option value='0' selected disabled>Categoria</option>
+          <option value='Aluno' selected>Aluno</option>
+          <option value='Responsavel'>Responsável</option>
+          <option value='Administrador'>Administrador</option>
+          <option value='Professor'>Professor</option>
+        </select>
+      </fieldset>
+      <fieldset>
+        <label for='select-genero-aluno' class='form-label'>Gênero do Aluno</label>
+        <select class='form-control form-select' name='select-genero-aluno' id='select-genero-aluno' required>
+          <option value='0' selected disabled>Gênero</option>
+          <option value='Masculino' selected>Masculino</option>
+          <option value='Feminino'>Feminino</option>
+        </select>
+      </fieldset>
+      <fieldset>
+        <label for='select-escolaridade' class='form-label'>Escolaridade do Aluno</label>
+        <select class='form-control form-select' name='select-escolaridade' id='select-escolaridade' required> 
+          <option value='-1' selected disabled>Escolaridade</option>
+          <option value='1'>1</option>
+          <option value='2'>2</option>
+          <option value='3'>3</option>
+          <option value='4'>4</option>
+          <option value='5'>5</option>
+        </select>
+      </fieldset>
+    ";
 
       break;
 
 
     case 'Professor':
       $html .= "
-        <fieldset>
-          <label for='select-categoria' class='form-label'>Categoria do Usuário</label>
-          <select class='form-control form-select' name='select-categoria' id='select-categoria' required>
-            <option value='0' disabled>Categoria</option>
-            <option value='Aluno'>Aluno</option>
-            <option value='Responsavel'>Responsável</option>
-            <option value='Administrador'>Administrador</option>
-            <option value='Professor' selected>Professor</option>
-          </select>
-        </fieldset>
-        <fieldset>
-          <label for='ipt-cpf' class='form-label'>CPF do Professor</label>
-          <input type='text' id='cpf-professor' class='form-control' value='{$dadosCompletos['cpf']}' required>
-        </fieldset>
-      ";
+      <fieldset>
+        <label for='select-categoria' class='form-label'>Categoria do Usuário</label>
+        <select class='form-control form-select' name='select-categoria' id='select-categoria' required>
+          <option value='0' disabled>Categoria</option>
+          <option value='Aluno'>Aluno</option>
+          <option value='Responsavel'>Responsável</option>
+          <option value='Administrador'>Administrador</option>
+          <option value='Professor' selected>Professor</option>
+        </select>
+      </fieldset>
+      <fieldset>
+        <label for='ipt-cpf' class='form-label'>CPF do Professor</label>
+        <input type='text' id='cpf-professor' class='form-control' value='{$dadosCompletos['cpf']}' required>
+      </fieldset>
+    ";
 
       break;
 
     case 'Administrador':
       $html .= "
-        <fieldset>
-          <label for='ipt-cargo-adm' class='form-label'>Cargo do Administrador</label>
-          <input type='text' name='ipt-cargo-adm' id='ipt-cargo-adm' class='form-control' value='{$dadosCompletos['cargo']}' required>
-        </fieldset>
+      <fieldset>
+        <label for='ipt-cargo-adm' class='form-label'>Cargo do Administrador</label>
+        <input type='text' name='ipt-cargo-adm' id='ipt-cargo-adm' class='form-control' value='{$dadosCompletos['cargo']}' required>
+      </fieldset>
 
-        <fieldset>
-          <label for='select-categoria' class='form-label'>Categoria do Usuário</label>
-          <select class='form-control form-select' name='select-categoria' id='select-categoria' required>
-            <option value='0' disabled>Categoria</option>
-            <option value='Aluno'>Aluno</option>
-            <option value='Responsavel'>Responsável</option>
-            <option value='Administrador' selected>Administrador</option>
-            <option value='Professor'>Professor</option>
-          </select>
-        </fieldset>";
+      <fieldset>
+        <label for='select-categoria' class='form-label'>Categoria do Usuário</label>
+        <select class='form-control form-select' name='select-categoria' id='select-categoria' required>
+          <option value='0' disabled>Categoria</option>
+          <option value='Aluno'>Aluno</option>
+          <option value='Responsavel'>Responsável</option>
+          <option value='Administrador' selected>Administrador</option>
+          <option value='Professor'>Professor</option>
+        </select>
+      </fieldset>";
 
       break;
 
     case 'Responsavel':
       $html .= "
-        <fieldset>
-          <label for='ipt-cpf-responsavel' class='form-label'>CPF do Responsável</label>
-          <input type='text' name='ipt-cpf-responsavel' id='ipt-cpf-responsavel' class='form-control' value='{$dadosCompletos['cpf']}' required>
-        </fieldset>
+      <fieldset>
+        <label for='ipt-cpf-responsavel' class='form-label'>CPF do Responsável</label>
+        <input type='text' name='ipt-cpf-responsavel' id='ipt-cpf-responsavel' class='form-control' value='{$dadosCompletos['cpf']}' required>
+      </fieldset>
 
-        <fieldset>
-          <label for='ipt-telefone-responsavel' class='form-label'>Telefone do Responsável</label>
-          <input type='number' id='ipt-telefone-responsavel' name='ipt-telefone-responsavel' class='form-control' value='{$dadosCompletos['telefone']}' required>
-        </fieldset>
+      <fieldset>
+        <label for='ipt-telefone-responsavel' class='form-label'>Telefone do Responsável</label>
+        <input type='number' id='ipt-telefone-responsavel' name='ipt-telefone-responsavel' class='form-control' value='{$dadosCompletos['telefone']}' required>
+      </fieldset>
 
-        <fieldset>
-          <label for='select-categoria' class='form-label'>Categoria do Usuário</label>
-          <select class='form-control form-select' name='select-categoria' id='select-categoria' required>
-            <option value='0' disabled>Categoria</option>
-            <option value='Aluno' >Aluno</option>
-            <option value='Responsavel' selected>Responsável</option>
-            <option value='Administrador'>Administrador</option>
-            <option value='Professor'>Professor</option>
-          </select>
-        </fieldset>
-      ";
+      <fieldset>
+        <label for='select-categoria' class='form-label'>Categoria do Usuário</label>
+        <select class='form-control form-select' name='select-categoria' id='select-categoria' required>
+          <option value='0' disabled>Categoria</option>
+          <option value='Aluno' >Aluno</option>
+          <option value='Responsavel' selected>Responsável</option>
+          <option value='Administrador'>Administrador</option>
+          <option value='Professor'>Professor</option>
+        </select>
+      </fieldset>
+    ";
       break;
 
     default:
@@ -185,5 +185,3 @@ function montaEstruturaFormulario($dadosCompletos)
   $retorno = ["dadosCompletos" => $dadosCompletos, "html" => $html];
   echo json_encode($retorno);
 }
-
-// echo json_encode($dadosCompletos);
