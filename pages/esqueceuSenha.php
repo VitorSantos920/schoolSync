580ae9438952727dea1199658393796e8f802eea
Maria Natália <cardim.natalia@gmail.com>
2024-06-08 22:24:33 -0300

    Alterações na pagina de envio de email - botao de enviar email não tem link associado

--- pages/esqueceuSenha.php
+++ pages/esqueceuSenha.php
@@ -22,19 +22,26 @@ include_once "../db/config.php";
             <img src="../assets/img/logo_transparente.png" alt="SchoolSync" class="logo">
             <p class="logo-text">SchoolSync</p>
             <p class="esqueceuSenha-text">Recuperar Senha</p>
-            <p class="sub-text">Insira seu email abaixo para receber instruções sobre como redefinir sua senha.</p>
+            <p class="sub-text">Insira seu email abaixo para receber um login temporário dos administradores do sistema.</p>
             <form method="POST" action="send_reset_email.php">
                 <div class="input-box">
                     <label for="email">Email:</label>
                     <input type="email" id="email" name="email" placeholder="abc@email.com" class="example-login" required>
-                    <button type="submit">ENVIAR EMAIL</button>
-                    <p class="sub-text">Voltar para <a class="link" href="index.php">Login</a></p>
+                        <button id="redirectButton">ENVIAR EMAIL</button>
+                    <p class="sub-text">Voltar para <a class="link" href="../pages/index.php">Login</a></p>
                 </div>
             </form>
         </div>
     </main>
     <script src="../assets/js/jquery.min.js"></script>
     <script src="../assets/js/sweetalert2.all.min.js"></script>
+
+    <!-- Script para abrir a tela de pedidoSenha -->
+    <script>
+        document.getElementById("redirectButton").addEventListener("click", function() {
+            window.location.href = "../pages/pedidoSenha.php";
+        });
+    </script>
 </body>
 
 </html>
---
+++ pages/pedidoSenha.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../assets/css/global.css">
+    <link rel="stylesheet" href="../assets/css/login.css?<?= time() ?>">
+    <link rel="stylesheet" href="../assets/css/variables.css">
+    <title>Recuperação de Senha - SchoolSync</title>
+</head>
+
+<body>
+    <main class="d-flex align-items-center justify-content-center m-0">
+        <div class="login-box">
+            <img src="../assets/img/logo_transparente.png" alt="SchoolSync" class="logo">
+            <p class="logo-text">Recuperação de Senha</p>
+            <p class="sub-text">Recebemos sua solicitação de recuperação de senha. Você receberá um email com seu login temporário e instruções dentro de 24 horas.</p>
+            <a href="..pages/index.php" class="btn btn-primary mt-3">Voltar ao Login</a>
+        </div>
+    </main>
+</body>
+</html>
